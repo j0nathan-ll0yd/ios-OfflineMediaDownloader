9116146838fcf491343336e54ebfe9f8f1f72846
Jonathan Lloyd <webmaster@lifegames.org>
2019-09-15 18:50:10 -0700

    Alternative; without progress

--- Sandbox/ViewModels/FileCellViewModel.swift
+++ Sandbox/ViewModels/FileCellViewModel.swift
@@ -8,24 +8,10 @@ final class FileCellViewModel: ObservableObject, Identifiable {
     @Published public var progress: Int = 0
     @Published public var isDownloaded: Bool = false
     @Published public var isDownloading: Bool = false
-    private var observation: NSKeyValueObservation?
     private var location: URL
+    private var disposables = Set<AnyCancellable>()
     private let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-    
-    private lazy var task: URLSessionDownloadTask = {
-        let session = URLSession.shared
-        let task = session.downloadTask(with: self.file.fileUrl) { (tempLocation, _, _) in
-            debugPrint("file saved to: \(String(describing: tempLocation))")
-            debugPrint("saving to: \(String(describing: self.location))")
-            self.location = tempLocation!
-            DispatchQueue.main.async{
-                self.isDownloaded = true
-                self.isDownloading = false
-            }
-        }
-        return task
-    }()
-    
+
     public init(file: File) {
         self.file = file
         self.location = self.documentsPath.appendingPathComponent(file.fileUrl.lastPathComponent)
@@ -34,12 +20,34 @@ final class FileCellViewModel: ObservableObject, Identifiable {
     
     public func download() {
         self.isDownloading = true
-        self.observation = self.task.progress.observe(\.fractionCompleted) { (progress, _) in
-            DispatchQueue.main.async{
-                self.progress = Int(progress.fractionCompleted * 100)
-            }
-        }
-        self.task.resume()
+        URLSession.shared.dataTaskPublisher(for: self.file.fileUrl)
+        .handleEvents(receiveSubscription: { (subscription) in
+            print("Receive subscription")
+        }, receiveOutput: { output in
+            print("Received output: \(output)")
+        }, receiveCompletion: { _ in
+            print("Receive completion")
+        }, receiveCancel: {
+            print("Receive cancel")
+        }, receiveRequest: { demand in
+            print("Receive request: \(demand)")
+            self.isDownloading = true
+        })
+        .sink(receiveCompletion: { completion in
+                debugPrint(".sink() received the completion", String(describing: completion))
+                DispatchQueue.main.async {
+                    self.isDownloaded = true
+                }
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(let anError):
+                        print("received error: ", anError)
+                }
+        }, receiveValue: { someValue in
+            debugPrint(".sink() received \(someValue)")
+        })
+        .store(in: &disposables)
     }
     
     public func play() {
